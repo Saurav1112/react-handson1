afc148a2803c50de46ab3a2d7d6e8607c92f2d4d
Saurav <Sauravchaudhary939@gmail.com>
2023-04-28 21:19:41 +0530

    react handson1

---
+++ sample-react-app/src/App.js
@@ -0,0 +1,37 @@
+import { Component } from 'react';
+import './App.css';
+import {MyClassComponent} from "./Components/MyClassComponent"
+import { MyFunctionalComponent } from "./Components/MyFunctionalComponent";
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      showClass:false,
+      showFunc:false,
+    }
+  }
+  toggleShowClassComponent = () =>{
+    this.setState((prevState)=>({ showClass: !prevState.showClass}));
+  }
+
+  toggleShowFuncComponent = () =>{
+    this.setState((prevState)=>({ showFunc: !prevState.showFunc}));
+  }
+
+  render(){
+  return (
+    <>
+    <h1 className='Heading'>Styling Using Function and class based Component</h1>
+    <div className="Function-Heading" onClick={this.toggleShowFuncComponent}>To see Styling in Functional Component</div>
+    <div className='Class-Heading' onClick={this.toggleShowClassComponent}>To See Styling in Class Component</div>
+    { this.state.showClass && <MyClassComponent/>}
+   {this.state.showFunc && < MyFunctionalComponent/>}
+    </>
+  );
+}
+}
+
+
+export default App;
